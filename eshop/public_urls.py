aeef008566023e5f990642568f636d06c17d3416
Amine Abdennadher <amine.abdenader@gmail.com>
2025-04-05 09:05:37 +0100

    debug the url

--- eshop/public_urls.py
+++ eshop/public_urls.py
@@ -5,9 +5,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from base.views import HomeView
+from django.http import HttpResponse
+def HomeView(request):
+    return HttpResponse('hello world')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView, name="home"),
     path("accounts/", include("base.urls")),
     path('auth/', include('allauth.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
