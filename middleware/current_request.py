745f726cb4aebd6e70f8fcde60565ef9750cdc18
Amine Abdennadher <amine.abdenader@gmail.com>
2025-04-07 17:21:31 +0100

    test 2 for redirect from https to http

--- middleware/current_request.py
+++ middleware/current_request.py
@@ -1,16 +1,21 @@
-# middleware/current_request.py
-import threading
+# eshop/middleware/conditional_ssl.py
 
-_thread_locals = threading.local()
+from django.shortcuts import redirect
 
-def get_current_request():
-    return getattr(_thread_locals, 'request', None)
-
-class ThreadLocalMiddleware:
+class ConditionalSSLRedirectMiddleware:
+    """
+    Redirects to HTTPS only if the current tenant is the public tenant.
+    Otherwise, leaves the request as is.
+    """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        _thread_locals.request = request
-        response = self.get_response(request)
-        return response
+        # Check if tenant is available; django-tenants sets `request.tenant`
+        tenant = getattr(request, 'tenant', None)
+        # Check if this is the public tenant and if the request is not secure.
+        if tenant and tenant.schema_name == 'public' and not request.is_secure():
+            # Build the secure URL: replace 'http://' with 'https://'
+            secure_url = request.build_absolute_uri(request.get_full_path()).replace('http://', 'https://', 1)
+            return redirect(secure_url)
+        return self.get_response(request)
