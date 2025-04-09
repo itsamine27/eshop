ae258c4a5b5e7cbbba77552c143d6cfae9b659ec
Amine Abdennadher <amine.abdenader@gmail.com>
2025-04-09 16:43:42 +0100

    add a appname to use the namespace of the tenant name

--- eshop/urls.py
+++ eshop/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.urls import path, include
 
-
+app_name = 'eshop'
 urlpatterns = [
     
     path('', include(('products.urls', 'product'), namespace="product")),
