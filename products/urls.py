45aebdc757b8d614ed494aa070a227b491041789
Amine Abdennadher <amine.abdenader@gmail.com>
2025-04-13 22:26:43 +0100

    update the delet product

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ app_name="product"
 urlpatterns =[
     path('addproduct/', AddProduct.as_view(), name='product_creation'),
     path('updateproduct/<int:pk>/', UpdateProduct.as_view(), name='update_product'),
-    path('DeleteProduct/<int:pk>/', DeleteProduct.as_view(), name='delete_product'),
+    path('deleteproduct/<int:pk>/', DeleteProduct.as_view(), name='delete_product'),
     
     path('', AllProducts.as_view(), name="allproducts"),
 ]
