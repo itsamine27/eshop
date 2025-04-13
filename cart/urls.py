9c8b458251829a5a0e41f4f7e5929df0f0fc3df3
Amine Abdennadher <amine.abdenader@gmail.com>
2025-04-13 19:15:59 +0100

    try to solve try 1k

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import AddCart,AllProductCart,DeleteCart
-app_name='cart'
-urlpatterns=[
+from .views import AddCart, AllProductCart, DeleteCart
+
+app_name = 'cart'
+urlpatterns = [
     path('addcart/<int:pk>/', AddCart.as_view(), name='addtocart'),
     path('mycart/', AllProductCart.as_view(), name="allprocart"),
     path('clearcart/', DeleteCart.as_view(), name="clearcart"),
-    
 ]
--- eshop/urls.py
+++ eshop/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.urls import path, include
 
 app_name = 'eshop_ns'
 urlpatterns = [
-    
     path('', include(('products.urls', 'product'), namespace="product")),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('search/', include(('searchprod.urls', 'search'), namespace="search")),
-
-    
-] 
+]
